98547fa44cb05a25f027e46b323911ae220c8f0e
Ayoub Ali <ayoubzulfiqar3@gmail.com>
2022-08-23 02:12:59 -0700

    Update appExample.dart
    example

--- appExample.dart
+++ appExample.dart
@@ -1,92 +1,103 @@
-// for Responsive App
-import 'package:.../constants.dart';
 import 'package:flutter/material.dart';
+import 'package:responsiveness/app_config.dart';
+import 'package:responsiveness/responsive.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    final responsive = Responsive(
-      context: context,
-      deviceHeight: 875.428,
-      deviceWidth: 411.428,
-    );
-    return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Container(
-              width: 300,
-              // height: 200,
-              height: responsive.getHeight(height: 200),
-              color: Colors.red[500]!,
-            ),
-          ),
-          SizedBox(
-            height: responsive.getHeight(height: 50),
-          ),
-          Container(
-            // width: responsive.getWidth(width: 80),
-            // height: responsive.getHeight(height: 80),
-            padding: EdgeInsets.only(
-              left: responsive.getLeftPadding(padding: 20),
-              right: responsive.getRightPadding(padding: 20),
-              top: responsive.getTopPadding(padding: 20),
-              bottom: responsive.getBottomPadding(padding: 20),
-            ),
-            // margin: EdgeInsets.only(
-            //   left: responsive.getLeftPadding(padding: 20),
-            //   right: responsive.getRightPadding(padding: 20),
-            //   top: responsive.getTopPadding(padding: 20),
-            //   bottom: responsive.getBottomPadding(padding: 20),
-            // ),
-            // margin: const EdgeInsets.all(20),
+  State<HomePage> createState() => _HomePageState();
+}
 
-            color: Colors.deepOrange,
-            child: const Text(
-              "Purple",
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
+class _HomePageState extends State<HomePage> {
+  // -- this is  my app screens that i was working on
+  // double deviceHeight = 875.428;
+  // double deviceWidth = 411.428;
+
+  // default size for every type of app
+  double deviceHeight = 690;
+  double deviceWidth = 360;
+
+  // to set orientation
+  getOrientation() {
+    Orientation currentOrientation = MediaQuery.of(context).orientation;
+    if (currentOrientation == Orientation.portrait) {
+      setState(() {
+        deviceHeight = MediaQuery.of(context).size.height;
+      });
+    } else {
+      setState(() {
+        deviceHeight = MediaQuery.of(context).size.width;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Responsive rs = Responsive(
+      context: context,
+      deviceHeight: deviceHeight,
+      deviceWidth: deviceWidth,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Responsiveness"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: rs.setWidth(width: 100),
+              height: rs.setHeight(height: 200),
+              alignment: Alignment.center,
+              color: Colors.black,
+              child: Text(
+                "H",
+                style: TextStyle(
+                    fontSize: rs.setFontSize(fontSize: 20),
+                    color: Colors.white),
               ),
             ),
-          ),
-          SizedBox(
-            height: responsive.getHeight(height: 20),
-          ),
-          Center(
-            child: Text(
-              "TEXT",
-              textScaleFactor:
-                  responsive.getTextScaleFactor(textScaleFactor: 1),
-              style: TextStyle(
-                fontSize: responsive.getFontSize(fontSize: 30),
-                fontWeight: FontWeight.bold,
+            Container(
+              width: rs.setWidth(width: 100),
+              height: rs.setHeight(height: 200),
+              alignment: Alignment.center,
+              color: Colors.blue,
+              child: Text(
+                "ScalerPixel",
+                style: TextStyle(
+                    fontSize: rs.setFontSize(fontSize: 20),
+                    color: Colors.white),
               ),
             ),
-          ),
-          SizedBox(
-            height: responsive.getHeight(height: 20),
-          ),
-          Container(
-            padding: const EdgeInsets.only(
-              left: 20,
-              right: 20,
-              top: 20,
-              bottom: 20,
-            ),
-            color: Colors.deepOrange,
-            child: const Text(
-              "Purple",
-              style: TextStyle(
-                fontSize: 50,
-                fontWeight: FontWeight.bold,
+            Container(
+              width: 100,
+              height: 100,
+              alignment: Alignment.center,
+              color: rs.getResponsiveValue(
+                forLargeScreen: Colors.red,
+                forTabletScreen: Colors.pink,
+                forMediumScreen: Colors.green,
+                forShortScreen: Colors.yellow,
+                forMobLandScapeMode: Colors.blue,
+                context: context,
+              ),
+              child: Text(
+                rs.getResponsiveValue(
+                    context: context,
+                    forLargeScreen: 'LargeScreen',
+                    forShortScreen: 'ShortScreen',
+                    forMediumScreen: 'MediumScreen',
+                    forTabletScreen: 'Tablet',
+                    forMobLandScapeMode: 'LandScape'),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
