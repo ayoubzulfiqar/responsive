5f5db42c0940e5797ebeec5842a08c5cc6a7382d
Ayoub Ali <ayoubzulfiqar3@gmail.com>
2023-07-18 07:41:22 -0700

    Improved Example

--- example.dart
+++ example.dart
@@ -1,109 +1,68 @@
 import 'package:flutter/material.dart';
-import 'package:responsiveness/responsive.dart';
+import 'package:responsiveness/util.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  State<HomePage> createState() => _HomePageState();
+void main() {
+  runApp(const Responsiveness());
 }
 
-class _HomePageState extends State<HomePage> {
-  // -- this is  my app screens that i was working on
-  // double deviceHeight = 875.428;
-  // double deviceWidth = 411.428;
-
-  // default size for every type of app
-  double deviceHeight = 690;
-  double deviceWidth = 360;
+class Responsiveness extends StatelessWidget {
+  const Responsiveness({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-     // to set and orientation
-    Orientation currentOrientation = MediaQuery.of(context).orientation;
-    if (currentOrientation == Orientation.portrait) {
-      setState(() {
-        deviceWidth = MediaQuery.of(context).size.height;
-      });
-    } else {
-      setState(() {
-        deviceHeight = MediaQuery.of(context).size.width;
-      });
-    }
-
-    Responsive rs = Responsive(
-      context: context,
-      deviceHeight: deviceHeight,
-      deviceWidth: deviceWidth,
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Responsiveness",
+      home: Home(),
     );
+  }
+}
 
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final height = 50.sH(context);
+    print("Height:$height");
+    final width = 50.sW(context);
+    print("Width:$width");
+    final font = 10.sF(context);
+    print("Font: $font");
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Responsiveness"),
-      ),
       body: Center(
         child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            const SizedBox(
+              height: 30,
+            ),
+            // Responsive
             Container(
-              width: rs.setWidth(width: 100),
-              height: rs.setHeight(height: 200),
-              alignment: Alignment.center,
-              decoration: BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(
-                      Radius.circular(rs.setRadius(radius: 20)))),
-              child: Text(
-                "H",
-                style: TextStyle(
-                    fontSize: rs.setFontSize(fontSize: 20),
-                    color: Colors.white),
+              color: Colors.yellow,
+              height: height,
+              width: width,
+              child: Center(
+                child: Text(
+                  "R",
+                  style: TextStyle(fontSize: font),
+                ),
               ),
             ),
-            SizedBox(
-              height: rs.setHeightSpace(height: 20),
+            const SizedBox(
+              height: 30,
             ),
+            // Normal
             Container(
-              width: 100,
-              height: 200,
-              decoration: const BoxDecoration(
-                  color: Colors.black,
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-            ),
-            Container(
-              width: rs.setWidth(width: 100),
-              height: rs.setHeight(height: 200),
-              alignment: Alignment.center,
               color: Colors.blue,
-              child: Text(
-                "ScalerPixel",
-                style: TextStyle(
-                    fontSize: rs.setFontSize(fontSize: 20),
-                    color: Colors.white),
-              ),
-            ),
-            Container(
-              width: 100,
-              height: 100,
-              alignment: Alignment.center,
-              color: rs.getResponsiveValue(
-                forLargeScreen: Colors.red,
-                forTabletScreen: Colors.pink,
-                forMediumScreen: Colors.green,
-                forShortScreen: Colors.yellow,
-                forMobLandScapeMode: Colors.blue,
-                context: context,
-              ),
-              child: Text(
-                rs.getResponsiveValue(
-                    context: context,
-                    forLargeScreen: 'LargeScreen',
-                    forShortScreen: 'ShortScreen',
-                    forMediumScreen: 'MediumScreen',
-                    forTabletScreen: 'Tablet',
-                    forMobLandScapeMode: 'LandScape'),
+              height: 50,
+              width: 50,
+              child: const Center(
+                child: Text(
+                  "N",
+                  style: TextStyle(
+                    fontSize: 10,
+                  ),
+                ),
               ),
             ),
           ],
